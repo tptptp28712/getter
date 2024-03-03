6578bc5c2b2d7408581d6ff7445aaad3fc145a6f
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-03 20:48:35 +0900

    Update t.java

--- t.java
+++ t.java
@@ -1,1 +1,29 @@
+package getter;
 
+public class t {
+	public static void main(String[] args) {
+        // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
+        Pokemon pikachu = new Pokemon("動物名：ライオン","体長：2.1m","速度：80km/h");
+        // 使用時
+        System.out.println(pikachu.name);
+        System.out.println(pikachu.category);
+        System.out.println(pikachu.type);
+    }
+
+}
+
+class Pokemon {
+    // フィールド変数
+    public int number;
+    public String name;
+    public String category;
+    public String type;
+    public String description;
+    public Pokemon(String name, String category, String type) {
+       
+        this.name = name;
+        this.category = category;
+        this.type = type;
+       
+    }
+}
