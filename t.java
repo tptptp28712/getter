a6ca4169f306e9d9a207b207404621e6405d738b
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-04 15:50:41 +0900

    Update t.java

--- t.java
+++ t.java
@@ -1,29 +1,35 @@
 package getter;
-
+import java.util.*;
 public class t {
 	public static void main(String[] args) {
-        // 呼び出し（new した後で、引数へ与えられた値を元に Pokemon が生成されます）
-        Pokemon pikachu = new Pokemon("動物名：ライオン","体長：2.1m","速度：80km/h");
-        // 使用時
-        System.out.println(pikachu.name);
-        System.out.println(pikachu.category);
-        System.out.println(pikachu.type);
-    }
-
+        // 呼び出し 
+ Car car = new Car();
+   car.work();
+  }
+} 
+class Car {
+    // 呼び出し
+  Tanku tanku = new Tanku();
+  public void work(){
+      // 使用時
+       System.out.println(tanku.getGas());
+       System.out.println(tanku.getGas2());
+       System.out.println(tanku.getGas3());
+  }
 }
-
-class Pokemon {
-    // フィールド変数
-    public int number;
-    public String name;
-    public String category;
-    public String type;
-    public String description;
-    public Pokemon(String name, String category, String type) {
-       
-        this.name = name;
-        this.category = category;
-        this.type = type;
-       
-    }
-}
+ class Tanku {
+     // フィールド変数
+ private String name = "動物名：ライオン";
+ private String category = "体長：2.1m";
+ private String type = "速度：80km/h";
+ public String getGas(){
+   return this.name;
+ } 
+ public String getGas2(){
+   return this.category;
+ } 
+ public String getGas3(){
+   return this.type;
+ } 
+  
+ }
