830aa2b7fab3ce72486b7966cda207cf9ce37736
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-04 18:44:38 +0900

    Update t.java

--- t.java
+++ t.java
@@ -2,19 +2,23 @@ package getter;
 import java.util.*;
 public class t {
 	public static void main(String[] args) {
+	// 呼び出し
  Car car = new Car();
    car.work();
   }
 } 
 class Car {
+	 // 呼び出し
   Tanku tanku = new Tanku("動物名：ライオン","体長：2.1m","速度：80km/h");
   public void work(){
+	  // 使用時
        System.out.println(tanku.getGas());
        System.out.println(tanku.getGas2());
        System.out.println(tanku.getGas3());
   }
 }
  class Tanku {
+	// フィールド変数
  private String name;
  private String category;
  private String type;
